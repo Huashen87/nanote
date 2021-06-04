6c644cc53a04fb4ad8bfb11637cc1198afc173bd
Huashen87 <60507281+Huashen87@users.noreply.github.com>
2021-06-05 01:18:10 +0800

    Modified some css about markdown and fixed typos

--- frontend/src/components/Note/Note.tsx
+++ frontend/src/components/Note/Note.tsx
@@ -40,12 +40,12 @@ function Note(props: { id: string }) {
     },
     updatedAt: new Date(),
     content: `# Nanote
-    \n取名為 Nanote 的意思是 "Nano" + "note"，如奈米般微小的筆記。
+    \n>取名為 Nanote 的意思是 "Nano" + "note"，如奈米般微小的筆記。
     \n生活中可能有許多點子、靈感，或是待辦事項，需要快速記錄下來以免忘記，但沒有一個方便、能快速紀錄的地方;也可能會遇到許多之前已經解決的問題，但始終找不到當初留下的紀錄，若有妥善分類將能方便查詢。鑒於這些需求，我希望能夠打造一個簡單、快速、方便、輕量的筆記應用程式，讓自己能夠快速記錄一些瑣碎的事項，並能夠利用分類快速找到相關的筆記。
-    \n作為一名初階(菜鳥)工程師，我知道Nanote目前一定會有許多不足之處，我也會盡我所能地改進它，希望他有朝一日能夠成為很棒的筆記應用程式!
+    \n作為一名初階(菜鳥)工程師，我知道Nanote目前一定會有許多不足之處，我也會盡我所能地改進它，希望有朝一日它能夠成為很棒的筆記應用程式!
     \n**如果對於Nanote有任何建議或疑問，請透過我的Github或是Email與我聯繫。**
-    \n- [我的Github](https://github.com/Huashen87)
-    \n- [我的Email](mailto:HuashenCoding@gmail.com)
+    \n- [**我的Github**](https://github.com/Huashen87)
+    \n- [**我的Email**](mailto:HuashenCoding@gmail.com)
     `,
   };
 
